186800244d11b1fdfa19f6c99ededfa6f260f450
Hamidreza Montaseri <montaseri.hamidreza@gmail.com>
2017-10-25 21:12:40 +0330

    Rename task blueprint name

--- ftask/ftask.py
+++ ftask/ftask.py
@@ -1,11 +1,11 @@
 # all the importsom flask import Flask, render_template
 from models import db, Task
 from flask import Flask, render_template
-from views import tasks
+from views import taskapp
 # initialize instance of WSGI application
 # act as a central registry for the view functions, URL rules, template configs
 app = Flask(__name__)
-app.register_blueprint(tasks)
+app.register_blueprint(taskapp)
 ## include db name in URI; _HOST entry overwrites all others
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017'
 app.config['MONGODB_SETTINGS'] = {'db':'ftask', 'alias':'default'} #necessary to set default alias
