bf53f6f6003ff64069a4aa16f9578cf60019e664
Hamidreza Montaseri <montaseri.hamidreza@gmail.com>
2017-10-25 12:27:49 +0330

    Test model

--- ftask/models.py
+++ ftask/models.py
@@ -1,3 +1,11 @@
 from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
+
+class Task(db.Document):
+    title = db.StringField(max_length=20,required=True)
+    start = db.DateTimeField(required=True)
+    finish = db.DateTimeField()
+    project = db.StringField(max_length=20)
+    description = db.StringField()
+    tags = db.ListField(db.StringField(max_length=20))
