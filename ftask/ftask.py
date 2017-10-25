55ec33cc209ef9d1e644a414fdea4cf81d549f1c
Hamidreza Montaseri <montaseri.hamidreza@gmail.com>
2017-10-25 14:10:04 +0330

    Remove view functions to views.py

--- ftask/ftask.py
+++ ftask/ftask.py
@@ -1,11 +1,11 @@
 # all the importsom flask import Flask, render_template
 from models import db, Task
 from flask import Flask, render_template
-import datetime
+from views import tasks
 # initialize instance of WSGI application
 # act as a central registry for the view functions, URL rules, template configs
 app = Flask(__name__)
-
+app.register_blueprint(tasks)
 ## include db name in URI; _HOST entry overwrites all others
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017'
 app.config['MONGODB_SETTINGS'] = {'db':'ftask', 'alias':'default'} #necessary to set default alias
@@ -13,13 +13,6 @@ app.debug = True
 
 # initalize app with database
 db.init_app(app)
-
-
-@app.route("/")
-def index():
-    tsk1 = Task(title="finish dinner",start=datetime.datetime.now())
-    tsk1.save()
-    return "hello!"
 
 
 if __name__ == "__main__":
